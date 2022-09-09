a08cba58949b21c9836f36baa48a325875ff9e6d
Laura <lauraabdulalim@gmail.com>
2022-09-10 00:02:48 +0200

    new

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ import os
 #file_path=os.path.join(os.getcwd(),(movies__final_data.csv))
 #df=pd.read_csv(file_path)
 
-df=pd.read_csv(r'D:\ENGINEERING\recommendation system\movies__final_data.csv')
+#df=pd.read_csv(r'D:\ENGINEERING\recommendation system\movies__final_data.csv')
 
 
 # In[86]:
