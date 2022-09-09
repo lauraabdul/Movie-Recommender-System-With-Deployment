79715a7508c0b33195e1d548d674694ee399ec1e
Laura <lauraabdulalim@gmail.com>
2022-09-09 15:37:08 +0200

    first

---
+++ router.py
@@ -0,0 +1,49 @@
+# Import the Libraries
+import flask
+import pandas as pd 
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, redirect, render_template, request
+
+import os
+
+from utils import get_recommendation ,cosine_sim
+
+app = Flask(__name__)
+
+
+df=pd.read_csv(r'D:\ENGINEERING\recommendation system\movies__final_data.csv')
+# create array with all movie titles
+all_titles = [df['title'][i] for i in range(len(df['title']))]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route for Predict page
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':  # while prediction
+
+        m_name = " ".join(flask.request.form['movie_name'].split())
+        if m_name not in all_titles:
+            return(flask.render_template('notfound.html')) #######
+        else:
+            result_final = get_recommendation(m_name)
+            names = []
+            for i in range(len(result_final)):
+                names.append(result_final.iloc[i])
+            return flask.render_template('predict.html',movie_names=names)
+    else:
+        return flask.render_template('predict.html') 
+
+
+
+
+# Run the App from the Terminal
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[79]:
+
+
+import pandas as pd 
+import numpy as np
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# In[80]:
+
+
+df=pd.read_csv(r'D:\ENGINEERING\recommendation system\movies__final_data.csv')
+
+
+# In[86]:
+
+
+def cosine_sim(df):
+    vectorizer=CountVectorizer(stop_words='english')
+    count_matrix=vectorizer.fit_transform(df['document'])
+    cos_sim=cosine_similarity(count_matrix,count_matrix)
+    df=df.reset_index()
+    indexs=pd.Series(df.index,df['title'])
+    return indexs , cos_sim
+
+
+# In[87]:
+
+
+def get_recommendation (title):
+    indexs,cos_sim=cosine_sim(df)
+    idx=indexs[title]
+    similarity_scores=list(enumerate(cos_sim[idx]))
+    similarity_scores=sorted(similarity_scores,key=lambda x: x[1],reverse=True) #key with 
+    #lambda function sorts according to the score itself as second element in each iteration in list
+    similarity_scores=similarity_scores[1:11] #10 movies
+    
+    movies_idx=[ind[0] for ind in similarity_scores]
+    movies=df['title'].iloc[movies_idx]
+    #movies_list=[ind for ind in movies]
+
+    return movies
+
+
+# In[ ]:
+
+
+
+# In[ ]:
+
+
+
+
